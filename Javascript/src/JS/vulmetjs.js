8006335ca8479c707a18bb382225e516d9dac9d5
SupHub <42800723+SupHub@users.noreply.github.com>
2019-03-15 19:29:33 +0100

    Add files via upload

---
+++ Javascript/src/JS/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+document.getElementById("sneaker1").src = "img/blackpair.jpg";
+document.getElementById("sneaker2").src = "img/whitepair.jpg";
