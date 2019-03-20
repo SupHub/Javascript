32758a44ec33050b07b6e62d98c8275231117a73
SupHub <42800723+SupHub@users.noreply.github.com>
2019-03-20 14:35:14 +0100

    Add files via upload

---
+++ Javascript/src/2.2blokjes.js
@@ -0,0 +1,18 @@
+let divje=document.getElementById( elementId= "bl1");
+divje.style.backgroundColor ="aqua";
+divje.style.padding ="25px";
+divje.style.paddingTop ="15px";
+
+let div=document.getElementById( elementId= "bl2");
+div.style.backgroundColor ="green";
+div.style.marginTop = "19px";
+div.style.padding="25px";
+
+let di=document.getElementById( elementId="bl3");
+di.style.backgroundColor ="yellow";
+di.style.marginTop = "54px";
+di.style.paddingLeft = "45px";
+
+let d=document.getElementById( element="bl4");
+d.style.backgroundColor = "red";
+d.style.paddingRight = "45px";
